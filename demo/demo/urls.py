dbfb1fe0e7b6d0a76ac7b337c568136bcfa6214d
Nathan Smith <nsmith13@nd.edu>
2013-05-17 19:42:08 -0400

    added polls models

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -4,14 +4,20 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse_lazy
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'demo.views.home', name='home'),
     # url(r'^demo/', include('demo.foo.urls')),
 
+    url(r'^$', RedirectView.as_view(url='sphinx_demo/', permanent=False)),
+    url(r'^sphinx_demo/', include('sphinx_demo.urls')),
+
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
