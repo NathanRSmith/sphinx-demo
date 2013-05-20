ffaabee488aea6469c8591f60dbcec91fed482d5
Nathan Smith <nsmith13@nd.edu>
2013-05-20 11:51:41 -0400

    added makefiles

--- doc/make.bat
+++ doc/make.bat
@@ -115,9 +115,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\Sphinx-Demo.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\blah.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\Sphinx-Demo.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\blah.ghc
 	goto end
 )
 
